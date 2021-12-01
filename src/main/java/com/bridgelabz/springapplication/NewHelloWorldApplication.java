5514dfa3097a5bb2c26d2b51596d25e4182ede1f
sonali2495 <sonaligadge24@gmail.com>
2021-12-02 01:21:43 +0530

    Initialized Spring Application

---
+++ src/main/java/com/bridgelabz/springapplication/NewHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewHelloWorldApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Bridgelabz");
+        SpringApplication.run(NewHelloWorldApplication.class, args);
+    }
+}
